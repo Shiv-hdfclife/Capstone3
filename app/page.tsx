76b24bbf0b26abb853fe79e32e75b80aa0243304
SHIVPRASADC_hdfclife <SHIVPRASAD.CHAVARATTIL@HDFCLIFE.COM>
2025-11-12 11:51:36 +0530

    Folder structure defines

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+'use client'
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div>
-      I am the best coder in this world :]
-    </div>
-  );
+  redirect("/login")
 }
+
